a49d702e66567af93eccf3ad4efb66cae146edbc
Dan Hamp <dhamp@solspec.io>
2020-02-27 08:27:06 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,10 +19,6 @@ chown -R tomcat:tomcat ${GEOSERVER_EXT_DIR}
 for ext in `ls -d "${GEOSERVER_EXT_DIR}"/*/`; do
     su tomcat -c "cp "${ext}"*.jar /usr/local/geoserver/WEB-INF/lib"
 done
-
-su tomcat -c "/usr/local/tomcat/bin/catalina.sh run"
-sleep 20
-pkill -9 tomcat
-pkill -9 catalina.sh
 python /user/local/bin/keycloak_config.py
 su tomcat -c "/usr/local/tomcat/bin/catalina.sh run"
+
