9a54db3b3c51b5b5afc78d863249b5da5c5b8b36
Dan Hamp <dhamp@solspec.io>
2020-03-26 13:22:42 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 # Check for Existance of geoserver install, if not then move into place
 if [ ! -d "${GEOSERVER_DATA_DIR}/logging.xml" ]; then
-    mv ${GEOSERVER_DATA_DIR} ${GEOSERVER_DATA_DIR_1}
-    mv ${GEOSERVER_EXT_DIR} ${GEOSERVER_EXT_DIR_1}
+    mv -r "${GEOSERVER_DATA_DIR}/*" "${GEOSERVER_DATA_DIR_1}/"
+    mv -r "${GEOSERVER_EXT_DIR}/*" "${GEOSERVER_EXT_DIR_1}/"
 fi
 
 if [ -n "${CUSTOM_UID}" ]; then
